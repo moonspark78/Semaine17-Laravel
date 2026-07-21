fd1bf9d20b589bcc41813c78349950070a1fb669
Omar Jabour <jabour.omar@hotmail.com>
2026-07-21 14:25:26 +0200

    J-2 NeW TP  create the Layout View .  :art:

---
+++ entreprise-semaine17/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+@include('partials.navbar')
+
+    <div class="container py-4">
+    {{-- Contenu de mes pages --}}
+    @yield('content')
+    </div>
+
+
+@include('partials.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
