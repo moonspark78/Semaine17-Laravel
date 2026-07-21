a6a34d133f88a366acbd3b98fb030410c1735040
Omar Jabour <jabour.omar@hotmail.com>
2026-07-21 14:23:25 +0200

    J-2 NeW TP  create the Layout View ./././  :art:

---
+++ entreprise-semaine17/resources/views/components/employe-card.blade.php
@@ -0,0 +1,16 @@
+<div class="card shadow-sm mb-3">
+    <div class="card-body">
+        <h5>
+            {{ $prenom }}
+            {{ $nom }}
+        </h5>
+        <p>
+            Service :
+            {{ $service }}
+        </p>
+        <p>
+            Salaire :
+            {{ $salaire }} €
+        </p>
+    </div>
+</div>
