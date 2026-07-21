bc7b763ff2ed36047425f642eb5b3f67d58dcf5b
Omar Jabour <jabour.omar@hotmail.com>
2026-07-21 14:29:40 +0200

    J-2 NeW TP  Pages Blades   ././   :art:

---
+++ entreprise-semaine17/resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+    <footer class="bg-light border-top py-3 mt-5 fixed-bottom">
+        <div class="container text-center">
+            © {{ date('Y') }} Entreprise Poseidon
+        </div>
+    </footer>
