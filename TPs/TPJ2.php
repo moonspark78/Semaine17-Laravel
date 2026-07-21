6d73c090f47d98cbc89b4149b4f39d1508a23fd0
Omar Jabour <jabour.omar@hotmail.com>
2026-07-21 14:14:30 +0200

    J-2 NeW TP :art:

---
+++ TPs/TPJ2.php
@@ -0,0 +1,60 @@
+<?php 
+
+/* 
+
+--- TP Jour 2 - Blade & Components ---
+
+
+--- Exercices manipulation Blade
+-- Dans une vue exercice, réponse aux questions suivantes : 
+
+- Exercice 1
+Afficher la variable
+$entreprise="MyEntreprise";
+dans un <h1>.
+
+- Exercice 2
+Afficher le nombre d'employés.
+
+- Exercice 3
+Afficher tous les prénoms.
+
+- Exercice 4
+Afficher :
+Nom - Prénom
+
+- Exercice 5
+Afficher uniquement les employés du service Informatique.
+
+- Exercice 6
+Afficher les salaires supérieurs à 2500 € en vert.
+
+- Exercice 7
+Afficher les employés hommes en bleu et les femmes en rose.
+
+- Exercice 8
+Afficher un badge Bootstrap selon le service. (avec des couleurs eventuellement)
+
+- Exercice 9
+Si le tableau est vide
+Afficher
+Aucun employé
+
+- Exercice 10
+Créer un tableau Bootstrap affichant :
+Nom
+Prénom
+Service
+Salaire
+
+--- TP Module Services version Blade 
+En réutilisant le travail fournit sur service dans le TP J1, tout transformer en utilisant blade
+- Transformer toutes les instructions php natives en blade
+- Créer / Reprendre le layout
+- Créer / Reprendre les partials 
+- Créer un component ServiceCard
+- Ajouter une condition en fonction des services d'afficher un "badge" de couleur
+- Afficher le nombre total de services
+- Afficher en bas de page Dernière mise à jour : ....datedujour.... 
+
+*/
