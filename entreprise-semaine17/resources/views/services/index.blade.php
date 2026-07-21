323ea74127fce42ffa1adcb6865303df5b9da3eb
Omar Jabour <jabour.omar@hotmail.com>
2026-07-21 14:31:13 +0200

    J-2 NeW TP  Pages Blades   .   :art:

--- entreprise-semaine17/resources/views/services/index.blade.php
+++ entreprise-semaine17/resources/views/services/index.blade.php
@@ -1,34 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Services</title>
-</head>
-<body>
+@extends('layouts.app')
 
-<a href="{{route('bienvenue')}}">Accueil</a> |
-<a href="{{route('liste')}}">Employés</a> |
-<a href="{{route('index')}}">Services</a> |
-<a href="/contact">Contact</a>
+@section('title')
+    Liste des services
+@endsection
 
-<h1>Liste des services</h1>
-
-@foreach($services as $service)
-
-<p>Nom : {{$service["nom"]}}</p>
-
-<p>Responsable : {{$service["responsable"]}}</p>
-
-<p>Téléphone : {{$service["telephone"]}}</p>
-
-<a href="{{route('show', $service["id"])}}">
-Voir le service
-</a>
-
-<hr>
-
-@endforeach
-
-</body>
-</html>
+@section('content')
+    <ul>
+        <?php 
+// var_dump($services)
+foreach($services as $service){
+    echo "<li>";
+    echo $service["nom"] . " - Responsable : " . $service["responsable"];
+?>
+        <a href="{{ route('services.show', ['id' => $service['id']]) }}">Lien vers le service</a>
+        <?php
+    echo "</li>";
+}
+?>
+    </ul>
+    <ul>
+        @forelse($services as $service)
+            <li> {{ $service['nom'] }} - Responsable : {{ $service['responsable'] }}
+                <a href="{{ route('services.show', ['id' => $service['id']]) }}">Lien vers le service</a>
+            @empty
+                <p>Aucun service pour l'instant</p>
+        @endforelse
+    </ul>
+@endsection
