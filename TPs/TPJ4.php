1f29e4ae3598206c98ff345f43d4ea3d6b16a914
Omar Jabour <jabour.omar@hotmail.com>
2026-07-23 11:46:43 +0200

    J-4 NeW-TP4:       :art:

---
+++ TPs/TPJ4.php
@@ -0,0 +1,12 @@
+<?php 
+
+/* 
+
+--- TP Jour 4 - CRUDer les services :) ---
+
+-- Création du Controller avec toutes les ressources 
+-- Création de toute les routes 
+-- Création de toutes les vues 
+
+
+*/
